d43c651cc031feacc25bd6b42d750d508978bcb2
Nick DeRobertis <whoopnip@gmail.com>
2021-11-03 08:56:54 -0400

    Ignore generated folder

--- tests/generated/init_project/plbuild/templates/author.py
+++
@@ -1,2 +0,0 @@
-
-AUTHORS = ['Nick DeRobertis']
--- tests/generated/init_project/plbuild/templates/general_imports.py
+++
@@ -1,8 +0,0 @@
-import pyexlatex as pl
-import pyexlatex.table as lt
-import pyexlatex.presentation as lp
-import pyexlatex.graphics as lg
-import pyexlatex.layouts as ll
-
-import plbuild
-from plbuild.paths import images_path
