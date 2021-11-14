aa62e5e847188b08c444a261a8b0bb03a587450e
Nick DeRobertis <whoopnip@gmail.com>
2021-11-14 17:01:11 -0500

    Fix issues with custom templates and add one more local template location

--- plbuilder/templater.py
+++ plbuilder/templater.py
@@ -11,13 +11,14 @@ class TemplateData(TypedDict):
     title: str
 
 
-@lru_cache
-def get_environment(local_path: Optional[Union[str, Path]] = None) -> jinja2.Environment:
+def get_environment(
+    local_path: Optional[Union[str, Path]] = None
+) -> jinja2.Environment:
     path = Path(local_path or os.getcwd())
-    local_templates_path = path / "templates"
-    local_loader = jinja2.FileSystemLoader(local_templates_path)
+    plbuild_loader = jinja2.FileSystemLoader(path / "plbuild" / "templates")
+    local_loader = jinja2.FileSystemLoader(path / "templates")
     default_loader = jinja2.PackageLoader("plbuilder")
-    choice_loader = jinja2.ChoiceLoader([local_loader, default_loader])
+    choice_loader = jinja2.ChoiceLoader([plbuild_loader, local_loader, default_loader])
     return jinja2.Environment(loader=choice_loader)
 
 
